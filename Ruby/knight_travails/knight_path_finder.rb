58e60424f1262c98980e9a736c32d70d6bb32080
therealpj <prajjwaljuyal@gmail.com>
2020-03-03 23:28:41 +0530

    Wrote build_move_tree

--- Ruby/knight_travails/knight_path_finder.rb
+++ Ruby/knight_travails/knight_path_finder.rb
@@ -1,13 +1,16 @@
 require_relative 'polytreenode.rb'
-
+require 'byebug'
 class KnightPathFinder
     def initialize(start)
-        @root = PolyTreeNode.new(start)
+        @start = start
         @considered_positions = Set.new
+        @considered_positions.add(@start)
+        
+        # build_move_tree
     end
     
 
-    def KnightPathFinder::valid_moves(pos)
+    def self.valid_moves(pos)
         r, c = pos
         valid = []
         moves = [2, 1, -2, -1]
@@ -21,12 +24,41 @@ class KnightPathFinder
         valid
     end
 
+    def new_move_positions(pos)
+        valid = KnightPathFinder::valid_moves(pos)
+        new_moves = []
+        valid.each do |pos|
+            new_moves << pos unless @considered_positions.include?(pos)
+            @considered_positions.add(pos)
+        end
+        new_moves
+    end
 
-    def KnightPathFinder::valid_pos?(possible_pos)
+    def self.valid_pos?(possible_pos)
         r, c = possible_pos
         return false unless r.between?(0, 7)
         return false unless c.between?(0, 7)
         return true
     end
+
+   
+
+    def build_move_tree
+        start = PolyTreeNode.new(@start)
+        queue = [start]
+        while queue.length != 0
+            next_pos = queue.shift
+            moves = new_move_positions(next_pos.value)
+            moves.each do |move|
+                move_node = PolyTreeNode.new(move)
+                move_node.parent = next_pos
+                next_pos.add_child(move_node)
+                queue.push(move_node) 
+            
+            end
+        end
+        start
+    end
+
 
 end
