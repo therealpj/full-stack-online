2d531e25b83b6d673319015ca02065dfae17c8bc
therealpj <prajjwaljuyal@gmail.com>
2020-03-03 22:31:22 +0530

    Add class method valid_moves

--- Ruby/knight_travails/knight_path_finder.rb
+++ Ruby/knight_travails/knight_path_finder.rb
@@ -5,4 +5,28 @@ class KnightPathFinder
         @root = PolyTreeNode.new(start)
         @considered_positions = Set.new
     end
+    
+
+    def KnightPathFinder::valid_moves(pos)
+        r, c = pos
+        valid = []
+        moves = [2, 1, -2, -1]
+        moves.each do |m|
+            moves.each do |move|
+                next if m.abs == move.abs
+                possible_pos = [r + move, c + m]
+                valid << possible_pos if valid_pos?(possible_pos)
+            end
+        end
+        valid
+    end
+
+
+    def KnightPathFinder::valid_pos?(possible_pos)
+        r, c = possible_pos
+        return false unless r.between?(0, 7)
+        return false unless c.between?(0, 7)
+        return true
+    end
+
 end
