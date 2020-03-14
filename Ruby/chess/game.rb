929809b683ba8a22350e552ec39035af9aa437f6
therealpj <prajjwaljuyal@gmail.com>
2020-03-14 18:54:14 +0530

    Removed bug in game.rb

--- Ruby/chess/game.rb
+++ Ruby/chess/game.rb
@@ -13,18 +13,12 @@ class Game
     end
 
     def play
-
-        until @board.checkmate?(:white) || @board.checkmate?(:black)
+        until game_over
             @display.render
-            input = @current_player.make_move
-            while input == nil
-                @display.render
-                start_pos = ignore_movement(input)
-                @display.render
-                end_pos = ignore_movement(@current_player.make_move)
-                @display.render
-                @board.move_piece(start_pos, end_pos)
-            end
+            start_pos = get_input.dup
+            @display.render
+            end_pos = get_input.dup
+            @board.move_piece(start_pos, end_pos)
             swap_turn!
         end
         
@@ -32,17 +26,22 @@ class Game
         puts @current_player.color.to_s.capitalize + " WINS!"
     end
 
+    def get_input
+        input = @current_player.make_move
+        while input == nil
+            @display.render
+            input = @current_player.make_move
+        end
+        input 
+    end
+
+    def game_over
+        @board.checkmate?(:white) || @board.checkmate?(:black)
+    end
+
     def swap_turn!
         @current_player =  @current_player == @p2 ? @p1 : @p2
     end
 
-    # helper method that waits for user to press enter or space
-    def ignore_movement(input)
-        while input == nil
-            @display.render
-            input = @display.cursor.get_input
-        end
-        input
-    end
-
+    
 end
