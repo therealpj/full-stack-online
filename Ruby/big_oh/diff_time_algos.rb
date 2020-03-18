1b58dc6482be19fb46922c9077d600915679bd65
therealpj <prajjwaljuyal@gmail.com>
2020-03-19 00:10:07 +0530

    Add largest_contiguous_sum which finds largest continuous sum in O(n^2)

--- Ruby/big_oh/diff_time_algos.rb
+++ Ruby/big_oh/diff_time_algos.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 
 def my_min_quadratic(list)
     
@@ -28,3 +28,53 @@ end
 
 # a little better now.
 # p my_min_linear((1..1000000).to_a.shuffle)
+
+
+def find_max_sum(arrays)
+    max = arrays[0].sum
+    arrays.each do |arr|
+        sum = arr.sum
+        if sum > max
+            max = sum
+        end
+    end
+    max
+end
+
+
+
+def find_contiguous_sub_array(arr, len)
+    sub_arrays = []
+    while arr.length >= len
+        sub_array = []
+        len.times do |i|
+            sub_array << arr[i]
+        end
+        arr.shift
+        sub_arrays << sub_array
+    end
+    sub_arrays
+end
+
+# p find_contiguous_sub_array([1,2,3,4], 5)
+
+def largest_contigous_sum(arr)
+    length = arr.length
+    max = arr[0]
+    (1..length).each do |len|
+        sub_arrays = find_contiguous_sub_array(arr.clone, len)
+        arr_sum = find_max_sum(sub_arrays)
+        if arr_sum > max
+            max = arr_sum
+        end    
+    end
+    max
+end
+
+# p largest_contigous_sum([-5, -1, -3])
+
+
+
+
+
+
