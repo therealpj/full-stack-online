96f107ed51129438eacb0b6c3317428c18801078
therealpj <prajjwaljuyal@gmail.com>
2020-03-19 22:22:07 +0530

    StackQueue class to enqueue in O(1) time

--- Ruby/big_oh/min_max_stack_queue.rb
+++ Ruby/big_oh/min_max_stack_queue.rb
@@ -50,3 +50,45 @@ class Stack
     end
 
 end 
+
+class StackQueue
+    def initialize
+        @arr = Stack.new
+        @rev_arr = Stack.new
+    end
+
+    def enqueue(ele)
+        @arr.push(ele)
+    end
+
+    def dequeue
+        reversify if @rev_arr.empty?
+        @rev_arr.pop
+    end
+
+    private
+    def reversify
+        @rev_arr.push(@arr.pop) until @arr.empty?
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
