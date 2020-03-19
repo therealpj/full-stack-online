7918473f6b2486625667ffd65658b0ca8c670bbd
therealpj <prajjwaljuyal@gmail.com>
2020-03-20 01:44:53 +0530

    Add method for finding best max difference in a window

--- Ruby/big_oh/min_max_stack_queue.rb
+++ Ruby/big_oh/min_max_stack_queue.rb
@@ -141,15 +141,39 @@ class MinMaxStackQueue
     end
 end
 
+require 'byebug'
+def windowed_max_range(arr, window)
+    best_max = -1.0/0.0
+    idx = 0
+    something = idx
+    # debugger
+    while something < arr.length
+        q = MinMaxStackQueue.new
+
+        something = idx
+        
+        window.times do
+            q.enqueue(arr[something])
+            something += 1
+        end
+
+        idx += 1
+        max = q.max
+        min = q.min
+        cur_max = max - min
+        if best_max < cur_max
+            best_max = cur_max
+        end
+
+    end
+    best_max
+end
 
 
-
-
-
-
-
-
-
+p windowed_max_range([1, 0, 2, 5, 4, 8], 2) == 4 # 4, 8
+p windowed_max_range([1, 0, 2, 5, 4, 8], 3) == 5 # 0, 2, 5
+p windowed_max_range([1, 0, 2, 5, 4, 8], 4) == 6 # 2, 5, 4, 8
+p windowed_max_range([1, 3, 2, 5, 4, 8], 5) == 6 # 3, 2, 5, 4, 8
 
 
 
