19b6eed3fae9c40f109aea1c11980eaf0f9af0bd
therealpj <prajjwaljuyal@gmail.com>
2020-03-21 17:59:26 +0530

    Add framework for LRUCache

---
+++ Ruby/lru/lru_cache.rb
@@ -0,0 +1,19 @@
+class LRUCache
+    def initialize
+    end
+
+    # returns the number of elements currently in cache
+    def count
+    end
+
+    # adds element to cache according to LRU principle
+    def add(el)
+    end
+
+    # shows the items in the cache, with the LRU item first
+    def show
+    end
+
+    private
+
+end
