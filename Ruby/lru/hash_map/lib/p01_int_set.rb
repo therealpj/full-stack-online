3a18099d9766052c5edef38d9ef368c34f944e58
therealpj <prajjwaljuyal@gmail.com>
2020-03-21 20:05:46 +0530

    Implement ResizingIntSet

--- Ruby/lru/hash_map/lib/p01_int_set.rb
+++ Ruby/lru/hash_map/lib/p01_int_set.rb
@@ -70,18 +70,35 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if self[num].include?(num)
+      return false
+    else
+      if @count == num_buckets
+        resize!
+      end
+
+      self[num] << num
+      @count += 1
+      return true
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      @count -= 1
+      self[num].delete(num)
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    idx = num % num_buckets
+    @store[idx]
   end
 
   def num_buckets
@@ -89,5 +106,15 @@ class ResizingIntSet
   end
 
   def resize!
+    store_dup = Array.new(num_buckets * 2) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |num|
+        store_dup[num % (num_buckets * 2)] << num
+      end
+    end
+    @store = store_dup
   end
+  
 end
+
+
