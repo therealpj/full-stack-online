0bdd0f90032299c0dcafc563d67968f166b3cb05
therealpj <prajjwaljuyal@gmail.com>
2020-03-21 21:00:59 +0530

    Implement HashSet

--- Ruby/lru/hash_map/lib/p03_hash_set.rb
+++ Ruby/lru/hash_map/lib/p03_hash_set.rb
@@ -7,18 +7,31 @@ class HashSet
   end
 
   def insert(key)
+    if @count == num_buckets
+      resize!
+    end
+
+    self[key] << key
+    @count += 1
+    return true
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    idx = num.hash
+    @store[idx % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +39,12 @@ class HashSet
   end
 
   def resize!
+    @store_dup = Array.new(num_buckets * 2) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |ele|
+        @store_dup[ele.hash % (num_buckets * 2)] = ele
+      end
+    end
+    @store = @store_dup
   end
 end
