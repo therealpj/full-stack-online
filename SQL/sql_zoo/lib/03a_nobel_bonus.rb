8fe7dbab43af254a9aa601451ec7dd067e5fe355
therealpj <prajjwaljuyal@gmail.com>
2020-03-25 18:31:25 +0530

    Add stater files for sql_zoo
    Finish part 1 of sql_zoo
    
    Write queries for part 02
    
    Write queries for 03
    
    Finish 03a bonus query
    
    Write queries for sql_zoo

---
+++ SQL/sql_zoo/lib/03a_nobel_bonus.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+    SELECT
+      yr
+    FROM
+      nobels
+    WHERE
+      subject LIKE 'Physics'
+    EXCEPT
+    SELECT
+      yr
+    FROM
+      nobels
+    WHERE
+      subject LIKE 'Chemistry'
+  SQL
+end
