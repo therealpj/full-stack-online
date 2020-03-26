cf1fc0dd332c5096115e9ac2db27ec5912b0ada9
therealpj <prajjwaljuyal@gmail.com>
2020-03-27 02:44:55 +0530

    Finish bonus queries of 04 SQLZoo

--- SQL/sql_zoo/lib/04a_countries_bonus.rb
+++ SQL/sql_zoo/lib/04a_countries_bonus.rb
@@ -14,6 +14,19 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+    SELECT
+      name
+    FROM
+      countries
+    WHERE
+      gdp > (
+        SELECT
+          max(gdp)
+        FROM
+          countries
+        WHERE
+          continent LIKE 'Europe'
+      )
   SQL
 end
 
@@ -21,6 +34,17 @@ def largest_in_continent
   # Find the largest country (by area) in each continent. Show the continent,
   # name, and area.
   execute(<<-SQL)
+    SELECT
+      continent, name, area
+    FROM
+      countries
+    WHERE
+      area in (
+        SELECT
+          MAX(area)
+        FROM
+          countries GROUP BY continent
+      )
   SQL
 end
 
@@ -28,5 +52,20 @@ def large_neighbors
   # Some countries have populations more than three times that of any of their
   # neighbors (in the same continent). Give the countries and continents.
   execute(<<-SQL)
+    SELECT
+      name, continent
+    FROM
+      countries as first
+    WHERE
+      population / 3 >  ALL (
+        SELECT
+          population
+        FROM
+          countries as second
+        WHERE
+          first.continent = second.continent
+        AND
+          first.name != second.name
+      )
   SQL
 end
