237ccad8fd5b23b7aa830fdb6aa665374541f4d8
therealpj <prajjwaljuyal@gmail.com>
2020-03-27 03:07:08 +0530

    Write queries for 05 SQLZoo

--- SQL/sql_zoo/lib/05_aggregates.rb
+++ SQL/sql_zoo/lib/05_aggregates.rb
@@ -22,30 +22,56 @@ end
 def continents
   # List all the continents - just once each.
   execute(<<-SQL)
+    SELECT
+      DISTINCT continent
+    FROM
+      countries
   SQL
 end
 
 def africa_gdp
   # Give the total GDP of Africa.
   execute(<<-SQL)
+  SELECT
+    SUM(gdp)
+  FROM
+    countries
+  WHERE
+    continent = 'Africa'
   SQL
 end
 
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
+    SELECT
+      Count(name)
+    FROM
+      countries
+    WHERE
+      area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+    select sum(population) from countries where name in (
+      'France',
+      'Germany',
+      'Spain'
+    )
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+    SELECT
+      continent, COUNT(countries)
+    FROM
+      countries
+    GROUP BY continent
   SQL
 end
 
@@ -53,11 +79,18 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+    select continent, count(population) from countries
+    where population >= 10000000
+    group by continent
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+    select continent
+    from countries
+    group by continent
+    having sum(population) > 100000000
   SQL
 end
