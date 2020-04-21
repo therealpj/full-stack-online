106bf81e65ac40488b7fd032e92c3f24c3fe4590
Prajjwal Juyal <prajjwaljuyal@gmail.com>
2020-04-21 21:42:21 +0530

    Finish Phase III

--- SQL/active_record_lite/lib/03_associatable.rb
+++ SQL/active_record_lite/lib/03_associatable.rb
@@ -10,41 +10,72 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
   def table_name
-    # ...
+    self.class_name.downcase + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
+  attr_accessor :class_name, :foreign_key, :primary_key
   def initialize(name, options = {})
-    # ...
+    @class_name = name.to_s.camelcase
+    @foreign_key = (name.to_s.singularize + "_id").to_sym
+    @primary_key = "id".to_sym
+
+    options.each do |option, val|
+      self.send("#{option.to_s + "="}", val)
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
+  attr_accessor :class_name, :foreign_key, :primary_key
   def initialize(name, self_class_name, options = {})
-    # ...
+    @class_name = name.to_s.singularize.capitalize
+    @foreign_key = (self_class_name.to_s.downcase.singularize + "_id").to_sym
+    @primary_key = "id".to_sym
+
+    options.each do |option, val|
+      self.send("#{option.to_s+"="}", val)
+    end
   end
+
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => key_val).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
+
+      key_val = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => key_val)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {  }
+    @assoc_options
   end
 end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
